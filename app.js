d72551500bd2e5b4ebd27832fb7351b64ac16462
Johannes Brodwall <johannes@brodwall.com>
2015-09-26 00:09:20 +0200

    Using NeDB for server side persistence

--- app.js
+++ app.js
@@ -11,26 +11,40 @@ app.use(bodyParser.json());
 var eventSources = [];
 var talks = {};
 
-app.post('/talks', function(req, res) {
-    talks[req.body.id] = req.body;
-    res.status(200).end();
+var Datastore = require('nedb');
+var db = new Datastore({ filename: 'db/talks.json' });
 
-    eventSources.forEach(function(eventSource) {
-        eventSource.write('event: insert\n');
-        eventSource.write('id: ' + req.body.id + '\n');
-        eventSource.write('data: ' + JSON.stringify(req.body) + '\n\n');
+db.loadDatabase(function(err) {    // Callback is optional
+    app.post('/talks', function(req, res) {
+        talks[req.body.id] = req.body;
+        res.status(200).end();
+
+        db.insert(req.body);
+
+        eventSources.forEach(function(eventSource) {
+            eventSource.write('event: insert\n');
+            eventSource.write('id: ' + req.body.id + '\n');
+            eventSource.write('data: ' + JSON.stringify(req.body) + '\n\n');
+        });
     });
-});
 
-app.get('/talks/events', function(req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        Connection: 'keep-alive',
+    app.get('/talks/events', function(req, res) {
+        res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            Connection: 'keep-alive',
+        });
+        res.write('\n');
+
+        db.find({}).sort({createdAt: -1}).limit(1).exec(function(err, tail) {
+            console.log(err);
+            console.log(tail);
+            res.write('event: start\n');
+            res.write('data: ' + JSON.stringify({tail: tail[0].createdAt}) + '\n\n');
+        });
+
+        eventSources.push(res);
     });
-    res.write('\n');
-
-    eventSources.push(res);
 });
 
 module.exports = app;
